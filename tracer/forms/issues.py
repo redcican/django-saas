4e285489682277c83f9914ad88df7f54cd61d54e
Shikun Chen <cican17@gmail.com>
2022-02-04 17:10:51 +0100

    init issues pages

--- tracer/forms/issues.py
+++ tracer/forms/issues.py
@@ -1,4 +1,5 @@
 from django import forms
+from tracer import models
 from tracer.forms.bootstrap import BootstrapForm
 from tracer.models import Issues
 from bootstrap_datepicker_plus.widgets import DatePickerInput
@@ -21,3 +22,37 @@ class IssuesModelForm(BootstrapForm, forms.ModelForm):
             # specify date-frmat
             'end_date': DatePickerInput(),
         }
+        
+    def __init__(self,request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        
+        # 初始化时，设置默认值
+        # 1. 获取当前项目的所有问题类型
+        issues_type_list = [("", "Nothing selected")]
+        issues_type_object_list = models.IssuesType.objects.filter(project=request.tracer.project).values_list('id','title')
+        issues_type_list.extend(issues_type_object_list)
+        self.fields['issues_type'].choices = issues_type_list
+        
+        # 2. 获取当前项目的所有模块
+        module_list = [("", "Nothing selected")]
+        moduel_object_list = models.Module.objects.filter(project=request.tracer.project).values_list('id', 'title')
+        module_list.extend(moduel_object_list)
+        self.fields['module'].choices = module_list
+        
+        # 3. 指派给谁 和 关注者
+        # 找到当前项目的参与者和创建者
+        total_user_list = [(request.tracer.project.creator_id, request.tracer.project.creator.username), ]
+        project_user_list = models.ProjectUser.objects.filter(project=request.tracer.project).values_list('user_id','user__username')
+        
+        total_user_list.extend(project_user_list)
+        
+        self.fields['assign'].choices = [("", "Nothing selected")] + total_user_list
+        self.fields['attention'].choices = total_user_list
+        
+        
+        # 4。 获取当前项目的所有父级问题
+        parent_list = [("", "Nothing selected")]
+        parent_object_list = models.Issues.objects.filter(project=request.tracer.project).values_list('id', 'subject')
+        parent_list.extend(parent_object_list)
+        self.fields['parent'].choices =  parent_list
--- tracer/views/issues.py
+++ tracer/views/issues.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from tracer.forms.issues import IssuesModelForm
 
 def issues(request, project_id):
-    form = IssuesModelForm()
+    form = IssuesModelForm(request)
     
     return render(request, 'issues.html', {'form': form})
