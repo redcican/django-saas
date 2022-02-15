4caff8a9a563a16ab243724e87f50e12e67035e1
shikun chen <shikun.chen@stud.uni-due.de>
2022-02-15 15:58:57 +0100

    add project name for deletion

--- tracer/views/setting.py
+++ tracer/views/setting.py
@@ -10,17 +10,22 @@ def setting(request, project_id):
 
 def setting_delete(request, project_id):
     """Delete a project on setting page"""
+
+    original_project_name = request.tracer.project.name
     if request.method == 'GET':
-        return render(request, 'setting_delete.html')
+
+        return render(request, 'setting_delete.html', {'original_project_name': original_project_name})
     
     project_name = request.POST.get('project_name')
     
     if not project_name or project_name != request.tracer.project.name:
-        return render(request, 'setting_delete.html', {'error':'Wrong project name!'})
+        return render(request, 'setting_delete.html', {'error': 'Wrong project name!',
+                                                       'original_project_name': original_project_name})
     
     # 只有项目的创建者才能删除项目
     if request.tracer.user != request.tracer.project.creator:
-        return render(request, 'setting_delete.html', {'error':'You are not the creator of this project!'})
+        return render(request, 'setting_delete.html', {'error': 'You are not the creator of this project!',
+                                                       'original_project_name': original_project_name})
     
     # 删除项目
     # 1. 删除COS Bucket 
